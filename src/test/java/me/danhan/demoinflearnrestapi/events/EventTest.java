b5bb46c52f610558131687a12a1297f57e461979
danhandev <danhandev@gmail.com>
2023-01-06 05:41:32 +0900

    이벤트 도메인 구현

---
+++ src/test/java/me/danhan/demoinflearnrestapi/events/EventTest.java
@@ -0,0 +1,32 @@
+package me.danhan.demoinflearnrestapi.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class EventTest {
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        // Given
+        String name = "Event";
+        String description = "Spring";
+
+        // When
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
